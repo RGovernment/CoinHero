bdcc32e8643c7c3a3bd3a909d6348e65469870be
RGovernment <117322410+RGovernment@users.noreply.github.com>
2026-08-27 15:23:12 +0900

    [추가] 카드 베이스 이미지 추가
    1.카드 베이스 이미지 추가 및 프리팹화
    ㄴ 1-2 베이스 데이터 직렬화

--- Assets/Script/Card/Base/CardData.cs
+++ Assets/Script/Card/Base/CardData.cs
@@ -7,20 +7,24 @@ public class CardData : MonoBehaviour
     public Card cardData;
     public Character user;
 
-    // 이하 카드 데이터 처리 함수 추가
-    [Header("카드 데이터 관련")]
-    [Header("코인 관련")]
-    public CoinObject[] coinObject;
-    public GameObject coinSlot;
-
     [Header("카드 외형")]
     public Image itemImage;
     public GameObject starSlot;
     public Image starImage;
-    
+    public Image typeIcon;
+    /// <summary>
+    /// (int)CardType.x로 호출해 사용
+    /// </summary>
+    public Sprite[] typeIconSprite;
+
 
     [Header("카드 텍스트")]
+    public TextMeshProUGUI nameText;
     public TextMeshProUGUI valueText;
+    public TextMeshProUGUI coinValueText;
+    public TextMeshProUGUI coinCountText;
     public TextMeshProUGUI description;
-    
+
+    [Header("카드 이미지 전환용")]
+    public CanvasGroup canvasGroup;
 }
--- Assets/Script/Card/UI/HandManager.cs
+++ Assets/Script/Card/UI/HandManager.cs
@@ -1,7 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 using SF = UnityEngine.SerializeField;
 public class HandManager : MonoBehaviour
 {
-    CanvasGroup handCanvas;
-    
+    [Header("기본 참조")]
+    [SF] private CanvasGroup handCanvas;
+    [SF] private CardData baseCardObj;
+    [Header("드래그 앤 드롭 용 더미 객체")]
+    [SF] private CardData ClickCard;
+
+    /// <summary>
+    /// 전체 카드, 핸드, 덱, 묘지 카드는 여기서 주고받아 조작됨 
+    /// 특수한 능력을 제외하면 변경되지 않음
+    /// </summary>
+    private List<CardData> allCards;
+    /// <summary>
+    /// 핸드 카드
+    /// </summary>
+    private List<CardData> handCards;
+
+    /// <summary>
+    /// 덱 내 카드, 스택으로 순서 보장
+    /// </summary>
+    private Stack<CardData> deckCards;
+
+    /// <summary>
+    /// 버려진 카드
+    /// </summary>
+    private List<CardData> discardCards;
+
+    public void CreateHandCard(List<Card> data,Character user)
+    {
+        foreach (var item in data)
+        {
+            CardData card = new()
+            {
+                cardData = item,
+                user = user
+            };
+
+            allCards.Add(card);
+        }
+    }
 }
